25a734095d7081352614c4aa57f47a753309728e
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-03 04:43:43 -0500

    Update PathParameters.tsx

--- src/Labs/Lab3/PathParameters.tsx
+++ src/Labs/Lab3/PathParameters.tsx
@@ -1,25 +1,30 @@
 import { Routes, Route, Link, useParams } from "react-router-dom";
 
 function AddNumbers() {
+  // Extract path parameters
   let { a, b } = useParams();
+
+  // Calculate sum
   const sum = Number(a) + Number(b);
 
   return (
     <div>
       <h2>Add Path Parameters</h2>
-      <p>
-        {a} + {b} = {sum}
-      </p>
+      <p>{a} + {b} = {sum}</p>
     </div>
   );
 }
 
 export default function PathParameters() {
   return (
-    <div id="wd-path-parameters">
-      <h2>Path Parameters</h2>
-      <Link to="/add/1/2">1 + 2</Link> <br />
+    <div>
+      <h1>Path Parameters</h1>
+      {/* Links with dynamic parameters */}
+      <Link to="/add/1/2">1 + 2</Link>
+      <br />
       <Link to="/add/3/4">3 + 4</Link>
+
+      {/* Define routes */}
       <Routes>
         <Route path="/add/:a/:b" element={<AddNumbers />} />
       </Routes>
