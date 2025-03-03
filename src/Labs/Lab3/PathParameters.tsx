0274b97a9fa63528990e8bac3173dfae4f442ea9
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-03 04:07:33 -0500

    Update PathParameters.tsx

--- src/Labs/Lab3/PathParameters.tsx
+++ src/Labs/Lab3/PathParameters.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AddPathParameters from "./AddPathParameters";
 export default function PathParameters() {
   return (
