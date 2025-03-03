b3293a90252b5e9457d74af33546c30e7c8b2624
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-03 04:24:04 -0500

    Update PathParameters.tsx

--- src/Labs/Lab3/PathParameters.tsx
+++ src/Labs/Lab3/PathParameters.tsx
@@ -1,15 +1,28 @@
-import { Routes, Route, Link } from "react-router-dom";
-import AddPathParameters from "./AddPathParameters";
-export default function PathParameters() {
+import { Routes, Route, Link, useParams } from "react-router-dom";
+
+function AddNumbers() {
+  let { a, b } = useParams();
+  const sum = parseInt(a) + parseInt(b);
+
   return (
-   <div id="wd-path-parameters">
-    <h2>Path Parameters</h2>
-    <Link to="/Labs/Lab3/add/1/2">1 + 2</Link> <br />
-    <Link to="/Labs/Lab3/add/3/4">3 + 4</Link>
-    <Routes>
-      <Route path="add/:a/:b" element={<AddPathParameters />} />
-    </Routes>
-   </div>
+    <div>
+      <h2>Add Path Parameters</h2>
+      <p>
+        {a} + {b} = {sum}
+      </p>
+    </div>
   );
 }
 
+export default function PathParameters() {
+  return (
+    <div id="wd-path-parameters">
+      <h2>Path Parameters</h2>
+      <Link to="/Labs/Lab3/add/1/2">1 + 2</Link> <br />
+      <Link to="/Labs/Lab3/add/3/4">3 + 4</Link>
+      <Routes>
+        <Route path="add/:a/:b" element={<AddNumbers />} />
+      </Routes>
+    </div>
+  );
+}
