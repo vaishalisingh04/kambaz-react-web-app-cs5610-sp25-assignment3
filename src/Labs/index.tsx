f617a76b550c207e4c1302a0716bb56014011bc8
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-03 04:44:44 -0500

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,22 +1,30 @@
-import { Navigate, Route, Routes } from "react-router";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Lab1 from "./Lab1";
 import TOC from "./TOC";
 import Lab3 from "./Lab3";
 import Lab2 from "./Lab2";
+import PathParameters from "./PathParameters";
 
 export default function Labs() {
   return (
-    <div id="wd-labs">
-      <h1>Vaishali Singh</h1>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab3" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-      </Routes>
+    <HashRouter>
+      <div id="wd-labs">
+        <h1>Vaishali Singh</h1>
+        <h1>Labs</h1>
+        <TOC />
+        <Routes>
+          {/* Default route redirects to Lab3 */}
+          <Route path="/" element={<Navigate to="Lab3" />} />
 
-    </div>
-);}
+          {/* Individual lab routes */}
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3" element={<Lab3 />} />
+
+          {/* Dynamic routing for Path Parameters */}
+          <Route path="PathParameters/*" element={<PathParameters />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
